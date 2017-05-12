8cffbfbb4302f0e69ba0ceef18fdda9ef2409e48
Tomek Demczuk <tomekkomek@gmail.com>
2017-05-12 18:41:03 +0200

    6/14

--- 2D Rougelike/Assets/Scripts/GameManager.cs
+++ 2D Rougelike/Assets/Scripts/GameManager.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public BoardManager boardScript;
+
+    public static GameManager instance = null;
+    private int level = 3;
+
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
 	}
-	
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
