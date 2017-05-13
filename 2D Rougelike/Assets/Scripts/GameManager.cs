6135a3466f059860b576e1a4006c103e60d81f8b
Tomek Demczuk <tomekkomek@gmail.com>
2017-05-14 01:26:54 +0200

    11/14

--- 2D Rougelike/Assets/Scripts/GameManager.cs
+++ 2D Rougelike/Assets/Scripts/GameManager.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public float turnDelay = .1f;
     public BoardManager boardScript;
-
     public static GameManager instance = null;
+    public int playerFoodPoints = 100;
+    [HideInInspector]
+    public bool playerTurn;
+
     private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
 
 
 	// Use this for initialization
@@ -18,17 +24,48 @@ public class GameManager : MonoBehaviour {
             Destroy(gameObject);
 
         DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
         InitGame();
 	}
 
     void InitGame()
     {
+        enemies.Clear();
         boardScript.SetupScene(level);
     }
 
+    public void GameOver()
+    {
+        enabled = false;
+    }
 	// Update is called once per frame
 	void Update () {
-		
+        if (playerTurn || enemiesMoving)
+            return;
+
+        StartCoroutine(MoveEnemies());
 	}
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if(enemies.Count==0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+        playerTurn = true;
+        enemiesMoving = false;
+    }
 }
